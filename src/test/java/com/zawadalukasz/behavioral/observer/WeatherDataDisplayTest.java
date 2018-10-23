97edfab1fdd9c744d940d05e0303ab6ef91e2412
Lukasz184396 <zawadalukasz88@gmail.com>
2018-10-23 16:02:28 +0200

    Eliminated repeating of creation WeatherData in each test of WeatherDataDisplayTest

--- src/test/java/com/zawadalukasz/behavioral/observer/WeatherDataDisplayTest.java
+++ src/test/java/com/zawadalukasz/behavioral/observer/WeatherDataDisplayTest.java
@@ -3,14 +3,23 @@ package com.zawadalukasz.behavioral.observer;
 import com.zawadalukasz.behavioral.observer.impl.CurrentConditionsDisplay;
 import com.zawadalukasz.behavioral.observer.impl.StatisticDisplay;
 import com.zawadalukasz.behavioral.observer.impl.WeatherData;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class WeatherDataDisplayTest {
+
+    WeatherData weatherData;
+
+    @Before
+    public void before() {
+       weatherData = new WeatherData();
+    }
+
     @Test
     public void checkCurrentConditions() {
-        WeatherData weatherData = new WeatherData();
+
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
 
         weatherData.setMeasurements(50, 25, 1000);
@@ -21,7 +30,7 @@ public class WeatherDataDisplayTest {
     @Test
     public void checkStatisticDisplay() {
 
-        WeatherData weatherData = new WeatherData();
+
         StatisticDisplay statisticDisplay = new StatisticDisplay(weatherData);
 
         weatherData.setMeasurements(50, 25, 1000);
@@ -33,7 +42,6 @@ public class WeatherDataDisplayTest {
     @Test
     public void shouldNotNotifyRemovedObserver() {
 
-        WeatherData weatherData = new WeatherData();
         StatisticDisplay statisticDisplay = new StatisticDisplay(weatherData);
 
         weatherData.setMeasurements(50, 25, 1000);
